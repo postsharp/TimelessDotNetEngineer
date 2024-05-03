7361c1a699e827c9dd47da67d7384511b83cf5a6
Antonín Procházka <antonin@postsharp.net>
2024-05-03 17:22:01 +0200

    #34853 HttpClient architecture validation.

---
+++ polly/PollyUsage/PollyDependencyInjection/AvoidInstantiatingHttpClientFabric.cs
@@ -0,0 +1,13 @@
+﻿using Metalama.Extensions.Architecture.Fabrics;
+using Metalama.Extensions.Architecture.Predicates;
+using Metalama.Framework.Fabrics;
+
+namespace PollyDependencyInjection;
+
+internal class AvoidInstantiatingHttpClientFabric : ProjectFabric
+{
+    public override void AmendProject(IProjectAmender amender)
+    {
+        amender.Verify().SelectTypes(typeof(HttpClient)).SelectMany(t => t.Constructors).CannotBeUsedFrom(r => r.Always(), $"Use {nameof(IHttpClientFactory)} instead.");
+    }
+}
---
+++ polly/PollyUsage/PollyDependencyInjection/ClassUsingHttpClient.cs
@@ -0,0 +1,16 @@
+﻿namespace PollyDependencyInjection;
+
+internal class ClassUsingHttpClient
+{
+    public async Task<string> GetAsync(string url)
+    {
+        // Instantiating HttpClient without using IHttpClientFactory avoids the benefits of Polly.
+        // See AvoidInstantiatingHttpClientFabric for a way to prevent this.
+
+        var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+
+        return await response.Content.ReadAsStringAsync();
+    }
+}
