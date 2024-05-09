14c16b0ee4d63eac9bcde835b6aa309ab48c72ce
GaelFraiteur <gael@postsharp.net>
2024-05-09 07:13:58 +0200

    Added benchmark.

---
+++ memorycache/part1/MemoryCacheBenchmark/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Caching.Memory;
+
+BenchmarkRunner.Run<MemoryCacheTest>();
+
+public class MemoryCacheTest
+{
+    private const int n = 10000;
+    private readonly MemoryCache _memoryCache = new(new MemoryCacheOptions());
+    private readonly (string Key, object Item)[] _existingItems;
+    private readonly (string Key, object Item)[] _newItems;
+    private int _nextExistingIndex;
+    private int _nextNewIndex;
+    
+    public MemoryCacheTest()
+    {
+        this._existingItems = Enumerable.Range(0, n).Select(x => (x.ToString(), new object() )).ToArray();
+        this._newItems = Enumerable.Range(n, 2*n).Select(x => (x.ToString(), new object() )).ToArray();
+        
+        foreach (var item in this._existingItems)
+        {
+            this._memoryCache.Set(item.Key, item.Item);
+        }
+    }
+
+    
+    [Benchmark( Baseline = true )]
+    public void AllocateObject() => _ = new Object();
+
+    [Benchmark]
+    public void Overhead()
+    {
+        var index = Interlocked.Increment(ref _nextExistingIndex) % n;
+        _ = this._existingItems[index].Key;
+    }
+
+    [Benchmark]
+    public void MemoryCache_Get()
+    {
+        var index = Interlocked.Increment(ref _nextExistingIndex) % n;
+        
+        this._memoryCache.TryGetValue(this._existingItems[index].Key, out _);
+    }
+
+    [Benchmark]
+    public void MemoryCache_Set()
+    {
+        var index = Interlocked.Increment(ref _nextExistingIndex) % n;
+        
+        var item = this._existingItems[index];
+        this._memoryCache.Set(item.Key, item.Item);
+    }
+    
+    [Benchmark]
+    public void MemoryCache_Add()
+    {
+        var index = Interlocked.Increment(ref _nextNewIndex) % n;
+        
+        var item = this._newItems[index];
+        this._memoryCache.Set(item.Key, item.Item);
+    }
+}
