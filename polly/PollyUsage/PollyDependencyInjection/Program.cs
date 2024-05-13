79ce24e50466bdcec163806b11163f9307c86f15
Antonín Procházka <prochan2@users.noreply.github.com>
2024-05-13 17:00:53 +0200

    PollyDependencyInjection snippet marked

--- polly/PollyUsage/PollyDependencyInjection/Program.cs
+++ polly/PollyUsage/PollyDependencyInjection/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+// [<snippet ComponentSpecificApi>]
 const string httpClientName = "MyClient";
 
 var services = new ServiceCollection()
@@ -14,3 +15,4 @@ var clientFactory = services.GetRequiredService<IHttpClientFactory>();
 var client = clientFactory.CreateClient(httpClientName);
 var response = await client.GetAsync("http://localhost:52394/FailEveryOtherTime");
 Console.WriteLine(await response.Content.ReadAsStringAsync());
+// [<endsnippet ComponentSpecificApi>]
