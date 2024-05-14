240b82ae9a8e86361bdf46ccd872b8fa12070134
GaelFraiteur <gael@postsharp.net>
2024-05-14 17:27:22 +0200

    ResilientDbCommand: move code to partial file.

---
+++ polly/PollyUsage/PollyDecorator/ResilientDbCommand.Properties.cs
@@ -0,0 +1,53 @@
+﻿using Polly;
+using System.Data;
+using System.Data.Common;
+
+namespace PollyDecorator;
+
+public partial class ResilientDbCommand 
+{
+    public override string CommandText
+    {
+        get => this._underlyingCommand.CommandText;
+        set => this._underlyingCommand.CommandText = value;
+    }
+
+    public override int CommandTimeout
+    {
+        get => this._underlyingCommand.CommandTimeout;
+        set => this._underlyingCommand.CommandTimeout = value;
+    }
+
+    public override CommandType CommandType
+    {
+        get => this._underlyingCommand.CommandType;
+        set => this._underlyingCommand.CommandType = value;
+    }
+
+    public override bool DesignTimeVisible
+    {
+        get => this._underlyingCommand.DesignTimeVisible;
+        set => this._underlyingCommand.DesignTimeVisible = value;
+    }
+
+    public override UpdateRowSource UpdatedRowSource
+    {
+        get => this._underlyingCommand.UpdatedRowSource;
+        set => this._underlyingCommand.UpdatedRowSource = value;
+    }
+
+    protected override DbConnection? DbConnection
+    {
+        get => this._underlyingCommand.Connection;
+        set => this._underlyingCommand.Connection = value;
+    }
+
+    protected override DbParameterCollection DbParameterCollection => this._underlyingCommand.Parameters;
+
+    protected override DbTransaction? DbTransaction
+    {
+        get => this._underlyingCommand.Transaction;
+        set => this._underlyingCommand.Transaction = value;
+    }
+
+}
--- polly/PollyUsage/PollyDecorator/ResilientDbCommand.cs
+++ polly/PollyUsage/PollyDecorator/ResilientDbCommand.cs
@@ -4,7 +4,7 @@ using System.Data.Common;
 
 namespace PollyDecorator;
 
-public class ResilientDbCommand : DbCommand
+public partial class ResilientDbCommand : DbCommand
 {
     private readonly DbCommand _underlyingCommand;
     private readonly ResiliencePipeline _resiliencePipeline;
@@ -14,51 +14,7 @@ public class ResilientDbCommand : DbCommand
         this._underlyingCommand = underlyingCommand;
         this._resiliencePipeline = resiliencePipeline;
     }
-
-    public override string CommandText
-    {
-        get => this._underlyingCommand.CommandText;
-        set => this._underlyingCommand.CommandText = value;
-    }
-
-    public override int CommandTimeout
-    {
-        get => this._underlyingCommand.CommandTimeout;
-        set => this._underlyingCommand.CommandTimeout = value;
-    }
-
-    public override CommandType CommandType
-    {
-        get => this._underlyingCommand.CommandType;
-        set => this._underlyingCommand.CommandType = value;
-    }
-
-    public override bool DesignTimeVisible
-    {
-        get => this._underlyingCommand.DesignTimeVisible;
-        set => this._underlyingCommand.DesignTimeVisible = value;
-    }
-
-    public override UpdateRowSource UpdatedRowSource
-    {
-        get => this._underlyingCommand.UpdatedRowSource;
-        set => this._underlyingCommand.UpdatedRowSource = value;
-    }
-
-    protected override DbConnection? DbConnection
-    {
-        get => this._underlyingCommand.Connection;
-        set => this._underlyingCommand.Connection = value;
-    }
-
-    protected override DbParameterCollection DbParameterCollection => this._underlyingCommand.Parameters;
-
-    protected override DbTransaction? DbTransaction
-    {
-        get => this._underlyingCommand.Transaction;
-        set => this._underlyingCommand.Transaction = value;
-    }
-
+    
     public override int ExecuteNonQuery()
         => this._resiliencePipeline.Execute(this._underlyingCommand.ExecuteNonQuery);
 
