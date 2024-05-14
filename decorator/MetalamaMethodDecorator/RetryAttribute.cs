d4c04c193ab957ad94c141b99614375b87f6feb5
GaelFraiteur <gael@postsharp.net>
2024-05-14 15:34:31 +0200

    Simplifying.

--- decorator/MetalamaMethodDecorator/RetryAttribute.cs
+++ decorator/MetalamaMethodDecorator/RetryAttribute.cs
@@ -7,7 +7,6 @@ internal class RetryAttribute : OverrideMethodAspect
 
     public double Delay { get; set; } = 1000;
 
-    // Template for non-async methods.
     public override dynamic? OverrideMethod()
     {
         for (var i = 0;; i++)
@@ -21,41 +20,13 @@ internal class RetryAttribute : OverrideMethodAspect
                 var delay = Delay * Math.Pow(2, i + 1);
 
                 Console.WriteLine(
-                    $"Method {meta.Target.Method.DeclaringType.Name}.{meta.Target.Method} has failed on {e.GetType().Name}. Retrying in {delay / 1000} seconds... ({i + 1}/{Attempts})");
+                    $"Method {meta.Target.Method.DeclaringType.Name}.{meta.Target.Method} has failed " +
+                    $" on {e.GetType().Name}. Retrying in {delay / 1000} seconds... ({i + 1}/{Attempts})");
 
                 Thread.Sleep((int)delay);
             }
         }
     }
-
-    // Template for async methods.
-    public override async Task<dynamic?> OverrideAsyncMethod()
-    {
-        var cancellationTokenParameter
-            = meta.Target.Parameters.LastOrDefault(p => p.Type.Is(typeof(CancellationToken)));
-
-        for (var i = 0;; i++)
-        {
-            try
-            {
-                return await meta.ProceedAsync();
-            }
-            catch (Exception e) when (i < Attempts)
-            {
-                var delay = Delay * Math.Pow(2, i + 1);
-
-                Console.WriteLine(
-                    $"Method {meta.Target.Method.DeclaringType.Name}.{meta.Target.Method} has failed on {e.GetType().Name}. Retrying in {delay / 1000} seconds... ({i + 1}/{Attempts})");
-
-                if (cancellationTokenParameter != null)
-                {
-                    await Task.Delay((int)delay, cancellationTokenParameter.Value);
-                }
-                else
-                {
-                    await Task.Delay((int)delay);
-                }
-            }
-        }
-    }
+    
+    // TODO: Implement OverrideMethodAsync and call Task.Delay instead of Thread.Sleep.
 }
