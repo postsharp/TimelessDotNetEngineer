5b728b50bed312b5525aafacf9a3dd274d6ead94
GaelFraiteur <gael@postsharp.net>
2024-05-15 16:10:23 +0200

    Warnings.

--- src/logging-with-string-interpolation/LoggingWithInterpolation/LogCriticalInterpolatedStringHandler.cs
+++ src/logging-with-string-interpolation/LoggingWithInterpolation/LogCriticalInterpolatedStringHandler.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) SharpCrafters s.r.o. See the LICENSE.md file in the root directory of this repository root for details.
 
 using Microsoft.Extensions.Logging;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Text;
 
