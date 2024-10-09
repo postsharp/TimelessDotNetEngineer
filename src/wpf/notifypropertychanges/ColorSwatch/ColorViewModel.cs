d1370e740c4bdb9cda33bd50dbd7c799df2e2d31
Dario Macchi <dariomac@gmail.com>
2024-10-09 09:39:00 -0300

    First working version

---
+++ src/wpf/notifypropertychanges/ColorSwatch/ColorViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using Metalama.Patterns.Observability;
+
+namespace ColorSwatch
+{
+    [NotifyPropertyChangedAttribute]
+    public partial class ColorViewModel : INotifyPropertyChanged
+    {
+        private ColorModel _colorModel;
+
+        public ColorViewModel()
+        {
+            _colorModel = new ColorModel();
+            HexColor = "#FFFFFF";
+        }
+
+        public string HexColor
+        {
+            get { return _colorModel.HexColor; }
+            set
+            {
+                if (_colorModel.HexColor != value)
+                {
+                    _colorModel.HexColor = value;
+                    // Notify that BackgroundBrush changed due to the HexColor change
+                    OnPropertyChanged(nameof(BackgroundBrush));
+                }
+            }
+        }
+
+        public SolidColorBrush BackgroundBrush
+        {
+            get {
+                return ColorModel.ConvertToBrush(HexColor);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
