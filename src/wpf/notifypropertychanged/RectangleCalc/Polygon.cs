f52941df916f578278b803f9dc5dae68b26d6962
Dario Macchi <dariomac@gmail.com>
2024-10-15 22:56:28 -0300

    RectangleCalc app for story-35376

---
+++ src/wpf/notifypropertychanged/RectangleCalc/Polygon.cs
@@ -0,0 +1,20 @@
+﻿using Metalama.Patterns.Observability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    [Observable]
+    partial class Polygon
+    {
+        // This attribute represents a multiplier for dimensions
+        public double ScaleFactor { get; set; }
+
+        public Polygon() {
+            ScaleFactor = 1;
+        }
+    }
+}
---
+++ src/wpf/notifypropertychanged/RectangleCalc/RectangleCalcViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Metalama.Patterns.Observability;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+using System.Xml.Linq;
+
+namespace RectangleArea
+{
+    [Observable]
+    partial class RectangleCalcViewModel
+    {
+        public Rectangle Rectangle { get; set; } = new Rectangle(10, 5);
+
+        public double Area => this.Rectangle.Area;
+    }
+}
