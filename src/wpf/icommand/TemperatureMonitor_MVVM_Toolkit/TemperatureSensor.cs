3e7532cace219c189e19ed39b0b975ce0edb26be
Dario Macchi <dariomac@gmail.com>
2024-10-23 22:46:55 -0300

    Add MVVM toolkit version of the example

---
+++ src/wpf/icommand/TemperatureMonitor_MVVM_Toolkit/TemperatureSensor.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureMonitor
+{
+    public partial class TemperatureSensor : ObservableObject
+    {
+        public TemperatureSensor() {
+            IsEnabled = false;
+            Threshold = 25; // Default threshold
+        }
+
+        [ObservableProperty]
+        private bool _isEnabled;
+
+        [ObservableProperty]
+        private bool _isMeasuring;
+
+        [ObservableProperty]
+        private double _temperature;
+
+        [ObservableProperty]
+        public double _threshold;
+
+        public async Task<double> MeasureTemperature()
+        {
+            IsMeasuring = true;
+            // Simulate measuring the temperature
+            await Task.Delay(2000);
+            IsMeasuring = false;
+
+            Random random = new Random();
+            return random.Next(10, 36);
+        }
+    }
+
+}
---
+++ src/wpf/icommand/TemperatureMonitor_MVVM_Toolkit/TemperatureViewModel.cs
@@ -0,0 +1,114 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TemperatureMonitor
+{
+    public partial class TemperatureViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(MeasureTemperatureCommand))]
+        private TemperatureSensor _sensor;
+
+        partial void OnSensorChanging(TemperatureSensor? sensor)
+        {
+            this.UnsubscribeFromSensor();
+        }
+
+        partial void OnSensorChanged(TemperatureSensor sensor)
+        {
+            this.SubscribeToSensor(sensor);
+        }
+
+        // [<snippet ToggleTemperatureSensorCommand>] 
+        [RelayCommand]
+        public void ToggleTemperatureSensor()
+        {
+            this.Sensor.IsEnabled = !this.Sensor.IsEnabled;
+        }
+        // [<endsnippet ToggleTemperatureSensorCommand>]
+
+        [RelayCommand]
+        public void SetThreshold(double threshold)
+        {
+            this.Sensor.Threshold = threshold;
+        }
+
+        // [<snippet MeasureTemperatureCommand>]
+        [RelayCommand(CanExecute = nameof(CanMeasureTemperature))]
+        public async Task MeasureTemperature()
+        {
+            this.Sensor.Temperature = await this.Sensor.MeasureTemperature();
+        }
+
+        private bool CanMeasureTemperature => this.Sensor.IsEnabled && !this.Sensor.IsMeasuring;
+        // [<endsnippet MeasureTemperatureCommand>]
+
+        public TemperatureViewModel()
+        {
+            this.Sensor = new TemperatureSensor();
+            
+            Threshold = this.Sensor.Threshold;
+        }
+        public bool IsSensorEnabled => this.Sensor.IsEnabled;
+
+        [ObservableProperty]
+        private double _threshold;
+
+        public double CurrentTemperature => this.Sensor.Temperature;
+
+        public string TemperatureStatus
+        {
+            get
+            {
+                if (this.Sensor.Temperature > this.Sensor.Threshold)
+                {
+                    return "Temperature is above threshold!";
+                }
+                else
+                {
+                    return "Temperature is below threshold.";
+                }
+            }
+        }
+
+        private void SubscribeToSensor(TemperatureSensor value)
+        {
+            if (value != null)
+            {
+                value.PropertyChanged += this.HandleSensorPropertyChanged;
+            }
+        }
+
+        private void HandleSensorPropertyChanged(object? sender, PropertyChangedEventArgs e)
+        {
+            {
+                var propertyName = e.PropertyName;
+
+                if (propertyName is nameof(this.Sensor.IsEnabled))
+                {
+                    this.OnPropertyChanged(nameof(this.IsSensorEnabled));
+                    this.MeasureTemperatureCommand.NotifyCanExecuteChanged();
+                }
+
+                if (propertyName is nameof(this.Sensor.Temperature) or nameof(this.Sensor.Threshold))
+                {
+                    this.OnPropertyChanged(nameof(this.CurrentTemperature));
+                    this.OnPropertyChanged(nameof(this.TemperatureStatus));
+                }
+            }
+        }
+
+        private void UnsubscribeFromSensor()
+        {
+            if (this._sensor != null!)
+            {
+                this._sensor.PropertyChanged -= this.HandleSensorPropertyChanged;
+            }
+        }
+    }
+}
