8660934527ee8c86d81fa13dbca71211694a00c0
GaelFraiteur <gael@postsharp.net>
2024-12-03 16:29:16 +0100

    Isolating snippets.

--- src/patterns/factory/abstract-factory/Program.cs
+++ src/patterns/factory/abstract-factory/Program.cs
@@ -8,13 +8,19 @@ internal class Program
 {
     private static async Task Main( string[] args )
     {
+        // [<snippet Initialize>]
         var services = new ServiceCollection();
         services.AddSingleton<IStorageAdapterFactory, StorageAdapterFactory>();
         services.AddHttpClient();
+        // [<endsnippet Initialize>]
 
-        var factory = services.BuildServiceProvider().GetRequiredService<IStorageAdapterFactory>();
-
+        var serviceProvider = services.BuildServiceProvider();
+        
+        // [<snippet Consume>]
+        var factory = serviceProvider.GetRequiredService<IStorageAdapterFactory>();
         var storage = factory.CreateStorageAdapter( "https://www.google.com" );
+        // [<endsnippet Consume>]
+        
         await using var stream = await storage.OpenReadAsync();
         using var reader = new StreamReader( stream );
         Console.WriteLine( await reader.ReadToEndAsync() );
